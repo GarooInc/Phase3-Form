5b960386619a369d4fcdd1bb429f1f64ff753274
Jorge Calderón <jcalderon900610@gmail.com>
2026-02-03 18:05:50 -0500

    Placeholders Mapping Corrected

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import AppRouter from './routes/AppRouter.tsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import AppRouter from "./routes/AppRouter.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+    // <StrictMode>
     <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-  </StrictMode>,
-)
+        <AppRouter />
+    </BrowserRouter>,
+    // </StrictMode>,
+);
